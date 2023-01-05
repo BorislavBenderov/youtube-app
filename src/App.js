1344ad0e358119343cf5b8c7fb53503bf49cf528
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-05 17:27:23 +0200

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Feed, Navbar, VideoDetail, SearchFeed, ChannelDetail } from './components';
+
 export const App = () => {
     return (
-        <div>
-            Hello World
-        </div>
+        <BrowserRouter>
+            <Box sx={{ backgroundColor: '#000' }}>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Feed />} />
+                    <Route path="/video/:id" element={<VideoDetail />} />
+                    <Route path="/channel/:id" element={<ChannelDetail />} />
+                    <Route path="/search/:searchTerm" element={<SearchFeed />} />
+                </Routes>
+            </Box>
+        </BrowserRouter>
     );
 }
