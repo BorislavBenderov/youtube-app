3b52551391fe06e1569be3cf084540a9edbdb8e5
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-06 12:42:40 +0200

    Optional chaining for videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -7,7 +7,7 @@ export const Videos = ({ videos }) => {
     return (
         <div>
             <Stack direction='row' flexWrap="wrap" justifyContent='start' gap={2}>
-                {videos.map((item, idx) =>
+                {videos?.map((item, idx) =>
                     <Box key={idx}>
                         {item.id.videoId && <VideoCard video={item} />}
                         {item.id.channelId && <ChannelCard channelDetail={item} />}
