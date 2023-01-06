2f519d31d60b01860b29d75d963691ad9c4936cd
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-06 11:53:45 +0200

    Render videos and channel

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,19 @@
+import { Box, Stack } from "@mui/material";
+import { ChannelCard } from "./ChannelCard";
+import { VideoCard } from "./VideoCard";
+
 export const Videos = ({ videos }) => {
-    console.log(videos);
+
     return (
-        <div>Videos</div>
+        <div>
+            <Stack direction='row' flexWrap="wrap" justifyContent='start' gap={2}>
+                {videos.map((item, idx) =>
+                    <Box key={idx}>
+                        {item.id.videoId && <VideoCard video={item} />}
+                        {item.id.channelId && <ChannelCard channelDetail={item} />}
+                    </Box>
+                )}
+            </Stack>
+        </div>
     );
 }
